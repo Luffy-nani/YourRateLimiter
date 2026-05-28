b8b4a42ba1d63b54f5ee4127d225d1b12f941130
Luffy-nani <venkatedupuganti0311@gmail.com>
2026-05-28 09:31:02 +0000

    Added client endpoints

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -30,4 +30,58 @@ const registerClient = async (req, res) => {
   }
 }
 
-module.exports = registerClient
+const getClients=async(req,res)=>{
+    try{
+        return res.json({
+          appName:req.client.appName,
+          rules:req.client.rules,
+          isActive:req.client.isActive
+        });
+    }
+    catch(err){
+        res.status(500).json({error:err.message});
+    }
+}
+
+const updateRules = async (req, res) => {
+  try {
+    const { rules } = req.body
+    req.client.rules = rules
+    await req.client.save()
+    res.json({ message: 'Rules updated', rules: req.client.rules })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
+
+const deactivateClient=async(req,res)=>{
+    try{
+        req.client.isActive=false;
+        await req.client.save();
+        res.json({message:"Client deactivated"});
+    }
+    catch(err){
+      res.status(500).json({message:err.message});
+    }
+}
+
+const rotateApiKey = async (req, res) => {
+  try {
+    const apiKey = 'rl_' + crypto.randomBytes(32).toString('hex')
+    const hashedApiKey = await bcrypt.hash(apiKey, 10)
+    const keyPrefix = apiKey.substring(0, 10)
+
+    req.client.apiKey = hashedApiKey
+    req.client.keyPrefix = keyPrefix
+    await req.client.save()
+
+    res.json({ 
+      message: 'API key rotated successfully',
+      apiKey  // show new key once
+    })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
+
+module.exports = { registerClient, getClients, updateRules, deactivateClient,rotateApiKey}
--- src/routes/clientRoute.js
+++ src/routes/clientRoute.js
@@ -1,7 +1,12 @@
-const registerClient=require(`../controllers/clientController`);
 const express=require(`express`);
+const verifyApiKey = require("../middleware/verifyApiKey");
+const { registerClient, getClients, updateRules, deactivateClient,rotateApiKey } = require('../controllers/clientController');
 const router=express.Router();
 
 router.post('/register',registerClient);
+router.get('/me', verifyApiKey, getClients)
+router.put('/rules', verifyApiKey, updateRules)
+router.delete('/', verifyApiKey, deactivateClient)
+router.post('/rotate-key', verifyApiKey, rotateApiKey) // Not put because rotating a key is an action (Actually either works but yeah post is better)
 
 module.exports=router;
